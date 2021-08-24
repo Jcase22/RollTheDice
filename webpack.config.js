536e5cf2535c389643dcf8014f6e940cbeea67b8
Justin Case <justin.case1061@gmail.com>
2021-08-23 20:22:22 -0400

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+module.exports = {
+  entry: './client/index.js',
+  mode: "development",
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'), //this is the folder you want to save your bundle in
+  },
+
+ module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
